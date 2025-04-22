4dd06c096bb4b11ec9e7f4628aa44ba0a0f96458
Sam-MARTis <samanthmartis@gmail.com>
2025-04-22 16:16:12 +0530

    Added multithreading functiionality

--- Assignment3/q3/dimCalculator.jl
+++ Assignment3/q3/dimCalculator.jl
@@ -1,4 +1,5 @@
 using Plots
+using Printf
 
 println("\033c")
 mutable struct SearchSpace
@@ -15,11 +16,7 @@ function shrink_search_range!(ss::SearchSpace)
 end
 function perform_search!(ss::SearchSpace, arr::Vector{BigFloat})
     i_start = searchsortedfirst(arr, ss.search_range_low)
-
     i_end   = searchsortedlast(arr, ss.search_range_high)
-    # println("i_start: ", i_start)
-    # println("i_end: ", i_end)
-    
     len = max(i_end - i_start + 1, 0)
     push!(ss.length_values, len)
 end
@@ -35,35 +32,18 @@ r = BigFloat("3.5699456")
 
 logratio = BigFloat("0.2")
 
-iterations = 2000000
+iterations = 3000000
 convergence_iterations = 40000
-starting_range = BigFloat("0.4")
-data_range = 20
+starting_range = BigFloat("0.3")
+data_range = 40
+
+available_threads = Threads.nthreads()
+println("Available Threads: ", available_threads)
+
+d_final = BigFloat("0")
+d_lock = ReentrantLock()
 
 
-points = Vector{BigFloat}()
-
-
-
-x = BigFloat("0.1")
-for i in 1:iterations
-    global x
-    x = logisticmap(x, r)
-    if i > convergence_iterations
-        push!(points, x)
-    end
-end
-
-sorted_points::Vector{BigFloat} = sort(points)
-
-search_ranges = Vector{SearchSpace}()
-
-
-for i in 1:data_range
-    point = rand(sorted_points)
-    ss = SearchSpace(Vector{BigFloat}(), point-starting_range/2,  point+starting_range/2, logratio)
-    push!(search_ranges, ss)
-end
 
 function log_point_counts(ranges_collection::Vector{SearchSpace}, points::Vector{BigFloat})
     for ss in ranges_collection
@@ -76,31 +56,65 @@ function shrink_search_space(ranges_collection::Vector{SearchSpace})
     end
 end
 function get_corelation_dimension(ss::SearchSpace, log_ratio::BigFloat)
-    first_value = ss.length_values[2]
-    last_value = ss.length_values[7]
-    slope = (log(last_value) - log(first_value)) / 5
+    start_idx = 2
+    end_idx = 8
+    first_value = ss.length_values[start_idx]
+    last_value = ss.length_values[end_idx]
+    slope = (log(last_value) - log(first_value)) / (end_idx - start_idx)
     d = slope/ log(log_ratio)
     return d
 end
-
-
-@assert issorted(sorted_points)
-sorted_points = unique(sorted_points)
-shrink_iterations = 15
-for i in 1:shrink_iterations
-    log_point_counts(search_ranges, sorted_points)
-    shrink_search_space(search_ranges)
-end
-
-
 function compute_average_correlation_dimension(ranges_collection::Vector{SearchSpace}, log_ratio::BigFloat)
     total_dimension = BigFloat(0)
     for ss in ranges_collection
         total_dimension += get_corelation_dimension(ss, log_ratio)
     end
     return total_dimension / length(ranges_collection)
-
 end
 
-println("Average Correlation Dimension: ", compute_average_correlation_dimension(search_ranges, logratio))
 
+Threads.@threads for i in 1:available_threads
+
+    points = Vector{BigFloat}()
+
+
+
+    local x = BigFloat(rand())
+    for i in 1:iterations
+        x = logisticmap(x, r)
+        if i > convergence_iterations
+            push!(points, x)
+        end
+    end
+
+
+
+    sort!(points)
+    search_ranges = Vector{SearchSpace}()
+
+    for i in 1:data_range
+        point = rand(points)
+        ss = SearchSpace(Vector{BigFloat}(), point-starting_range/2,  point+starting_range/2, logratio)
+        push!(search_ranges, ss)
+    end
+
+
+    # @assert issorted(points)
+    # points = unique(points)
+    sampling_iterations = 15
+    for i in 1:sampling_iterations
+        log_point_counts(search_ranges, points)
+        shrink_search_space(search_ranges)
+    end
+
+
+
+    d = compute_average_correlation_dimension(search_ranges, logratio)
+    # println("Average Correlation Dimension: ", round(d, 5), " on Thread ", Threads.threadid())
+    @printf("Average Correlation Dimension: %.5f on Thread %d\n", d, Threads.threadid())
+    global d_final
+    @lock d_lock d_final += d
+end
+
+
+@printf("\n\nFinal Average Correlation Dimension: %.5f", d_final / available_threads)
