dbecac203ac8104bc61edd20bbc85020de1addce
Sam-MARTis <samanthmartis@gmail.com>
2025-04-22 15:09:10 +0530

    Completed correlation dimension calculation

--- Assignment3/q3/dimCalculator.jl
+++ Assignment3/q3/dimCalculator.jl
@@ -1,4 +1,4 @@
-
+using Plots
 
 println("\033c")
 mutable struct SearchSpace
@@ -14,8 +14,6 @@ function shrink_search_range!(ss::SearchSpace)
     ss.search_range_high = midpoint + Δrange / 2
 end
 function perform_search!(ss::SearchSpace, arr::Vector{BigFloat})
-    # println("SS_search_range_low: ", ss.search_range_low)
-    # println("SS_search_range_high: ", ss.search_range_high)
     i_start = searchsortedfirst(arr, ss.search_range_low)
 
     i_end   = searchsortedlast(arr, ss.search_range_high)
@@ -33,39 +31,42 @@ function logisticmap(x::BigFloat, r::BigFloat)::BigFloat
     return r * x * (1 - x)
 end
 
-x = BigFloat("0.4")
-r = BigFloat("3.5699457")
+r = BigFloat("3.5699456")
 
-logratio = BigFloat("0.5")
-iterations = 20000
-convergence_iterations = 400
-starting_range = BigFloat("0.1")
+logratio = BigFloat("0.2")
+
+iterations = 2000000
+convergence_iterations = 40000
+starting_range = BigFloat("0.4")
 data_range = 20
+
+
 points = Vector{BigFloat}()
+
+
+
+x = BigFloat("0.1")
 for i in 1:iterations
     global x
-    
     x = logisticmap(x, r)
     if i > convergence_iterations
         push!(points, x)
     end
 end
 
-# println("Number of points: ", length(points))
 sorted_points::Vector{BigFloat} = sort(points)
 
 search_ranges = Vector{SearchSpace}()
+
+
 for i in 1:data_range
-    # push!(search_ranges, BigFloat(i) / 100)
     point = rand(sorted_points)
     ss = SearchSpace(Vector{BigFloat}(), point-starting_range/2,  point+starting_range/2, logratio)
     push!(search_ranges, ss)
 end
 
 function log_point_counts(ranges_collection::Vector{SearchSpace}, points::Vector{BigFloat})
-    # println("Inside log_point_counts")
     for ss in ranges_collection
-        # println("Type of ss: ", typeof(ss))
         perform_search!(ss, points)
     end
 end
@@ -74,13 +75,32 @@ function shrink_search_space(ranges_collection::Vector{SearchSpace})
         shrink_search_range!(ss)
     end
 end
+function get_corelation_dimension(ss::SearchSpace, log_ratio::BigFloat)
+    first_value = ss.length_values[2]
+    last_value = ss.length_values[7]
+    slope = (log(last_value) - log(first_value)) / 5
+    d = slope/ log(log_ratio)
+    return d
+end
+
+
 @assert issorted(sorted_points)
 sorted_points = unique(sorted_points)
-for i in 1:data_range
+shrink_iterations = 15
+for i in 1:shrink_iterations
     log_point_counts(search_ranges, sorted_points)
     shrink_search_space(search_ranges)
 end
 
 
-ss2 = search_ranges[5]
-println("ss2: ", ss2.length_values)
+function compute_average_correlation_dimension(ranges_collection::Vector{SearchSpace}, log_ratio::BigFloat)
+    total_dimension = BigFloat(0)
+    for ss in ranges_collection
+        total_dimension += get_corelation_dimension(ss, log_ratio)
+    end
+    return total_dimension / length(ranges_collection)
+
+end
+
+println("Average Correlation Dimension: ", compute_average_correlation_dimension(search_ranges, logratio))
+
